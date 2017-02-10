4a22c2df3fe7680565329e6261ba51bda25cc9fd
Drew Hart <drew@drewhart.com>
2017-02-10 14:22:32 -0600

    added move right and move left functions

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const app = "I don't do much."
+console.log(`index.js loaded...`)
+function moveDodgerRight(x) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+
+    if (left < 360) {
+      dodger.style.left = `${left + x}px`
+    }
+}
+function moveDodgerLeft(x) {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - x}px`
+  }
+}
+
+var init = function() {
+//  document.addEventListener('keydown', function(e) { console.log(e.which) } )
+
+  var dodger = document.getElementById('dodger')
+
+  document.addEventListener('keydown', function(e) {
+    var distance = 1
+
+    if (e.which === 37) {
+   		moveDodgerLeft(distance)
+    }
+
+    if (e.which === 39) {
+      moveDodgerRight(distance)
+    }
+  });
+}
+
+window.addEventListener('load', init())
